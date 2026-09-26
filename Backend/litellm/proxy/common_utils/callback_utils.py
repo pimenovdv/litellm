e51422e041009a0aeba1eb431a9b47e24929bc49
pimenovdv <25670278+pimenovdv@users.noreply.github.com>
2026-09-26 19:17:50 +0000

    chore: end session completely

--- Backend/litellm/proxy/common_utils/callback_utils.py
+++ Backend/litellm/proxy/common_utils/callback_utils.py
@@ -90,7 +90,8 @@ def initialize_callbacks_on_proxy(
             if isinstance(callback, str) and callback in litellm._known_custom_logger_compatible_callbacks:
                 imported_list.append(callback)
             elif isinstance(callback, str) and callback == "presidio":
-                                    _OPTIONAL_PresidioPIIMasking,
+                from litellm.litellm_core_utils.pii_masking import (
+                    _OPTIONAL_PresidioPIIMasking,
                 )
 
                 presidio_logging_only: Optional[bool] = litellm_settings.get("presidio_logging_only", None)
@@ -154,7 +155,8 @@ def initialize_callbacks_on_proxy(
                 openai_moderations_object = _ENTERPRISE_OpenAI_Moderation()
                 imported_list.append(openai_moderations_object)
             elif isinstance(callback, str) and callback == "lakera_prompt_injection":
-                                    lakeraAI_Moderation,
+                from litellm.litellm_core_utils.prompt_injection_detection.lakera_ai import (
+                    lakeraAI_Moderation,
                 )
 
                 init_params = {}
@@ -165,7 +167,8 @@ def initialize_callbacks_on_proxy(
                 lakera_moderations_object = lakeraAI_Moderation(**init_params)
                 imported_list.append(lakera_moderations_object)
             elif isinstance(callback, str) and callback == "aporia_prompt_injection":
-                                    AporiaGuardrail,
+                from litellm.litellm_core_utils.prompt_injection_detection.aporia_ai import (
+                    AporiaGuardrail,
                 )
 
                 aporia_guardrail_object = AporiaGuardrail()
